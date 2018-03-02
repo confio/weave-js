c6c744d76110d24171f54df91a8a0060459aedb7
Ethan Frey <ethanfrey@users.noreply.github.com>
2018-03-02 16:18:40 +0100

    Add more proto files, test sendMsg

--- src/proto/index.js
+++ src/proto/index.js
@@ -1,30 +1,55 @@
 import protobuf from "protobufjs";
 import path from "path";
 
-let _acccountMessage = null;
+let _wallet = null;
+let _sendMsg = null;
 
-let protoPath = path.resolve(__dirname, "account.proto");
+let protoPath = path.resolve(__dirname, "mycoind.proto");
 
-async function accountClass() {
-    if (_acccountMessage == null) {
-        _acccountMessage = await protobuf.load(protoPath)
+async function walletClass() {
+    if (_wallet == null) {
+        _wallet = await protobuf.load(protoPath)
                                 .then(root => root.lookupType("mycoin.Set"))
                                 // .catch(err => console.log("Err:" + err));
     }
-    return _acccountMessage;
+    return _wallet;
 }
 
-export async function parseAccount(buffer) {
-    let Account = await accountClass();
-    let decodedMessage = Account.decode(buffer);
-    return Account.toObject(decodedMessage);
+async function sendMsgClass() {
+    if (_sendMsg == null) {
+        _sendMsg = await protobuf.load(protoPath)
+                                 .then(root => root.lookupType("mycoin.SendMsg"))
+                                // .catch(err => console.log("Err:" + err));
+    }
+    return _sendMsg;
 }
 
-export async function serializeAccount(obj) {
-    let Account = await accountClass();
-    let err = Account.verify(obj);
+export async function parseWallet(buffer) {
+    let Wallet = await walletClass();
+    let decodedMessage = Wallet.decode(buffer);
+    return Wallet.toObject(decodedMessage);
+}
+
+export async function serializeWallet(obj) {
+    let Wallet = await walletClass();
+    let err = Wallet.verify(obj);
     if (err) throw Error(err);
 
-    let buffer = Account.encode(obj).finish();
+    let buffer = Wallet.encode(obj).finish();
+    return buffer;
+}
+
+export async function parseSendMsg(buffer) {
+    let SendMsg = await sendMsgClass();
+    let decodedMessage = SendMsg.decode(buffer);
+    return SendMsg.toObject(decodedMessage, {bytes: String});
+}
+
+export async function serializeSendMsg(obj) {
+    let SendMsg = await sendMsgClass();
+    let err = SendMsg.verify(obj);
+    if (err) throw Error(err);
+
+    let buffer = SendMsg.encode(obj).finish();
     return buffer;
 }
--- test/proto.spec.js
+++ test/proto.spec.js
@@ -1,26 +1,51 @@
 import fs from "fs";
 import path from "path";
-import { parseAccount, serializeAccount } from '../src/proto';
+import { parseWallet, serializeWallet, parseSendMsg, serializeSendMsg } from '../src/proto';
 
-describe('Protobuf account', () => {
-    let accountJSON = path.resolve(__dirname, "data", "account.json");
-    let accountBin = path.resolve(__dirname, "data", "account.bin");
+describe('Protobuf wallet', () => {
+    let jsonWallet = loadJSON("wallet");
+    let hexWallet = loadPB("wallet");
 
-    // NOTE: go output uses currency_code, js currencyCode, must translate
-    let jsonAccount = fs.readFileSync(accountJSON, 'utf8')
-                        .replace(/currency_code/g, "currencyCode");
-    let hexAccount = fs.readFileSync(accountBin, 'hex');
+    let jsonMsg = loadJSON("send_msg");
+    let hexMsg = loadPB("send_msg");
 
-    it('Serialize json to protobuf', async () => {
-        let obj = JSON.parse(jsonAccount);
-        let buffer = await serializeAccount(obj);
+    it('Serialize json wallet to protobuf', async () => {
+        let obj = JSON.parse(jsonWallet);
+        let buffer = await serializeWallet(obj);
         let hex = buffer.toString('hex')
-        expect(hex).toEqual(hexAccount);
+        expect(hex).toEqual(hexWallet);
     });
-    it('Parse protobuf to json', async () => {
-        let buffer = new Buffer(hexAccount, 'hex');
-        let parsed = await parseAccount(buffer);
-        let obj = JSON.parse(jsonAccount);
+    it('Parse protobuf wallet to json', async () => {
+        let buffer = new Buffer(hexWallet, 'hex');
+        let parsed = await parseWallet(buffer);
+        let obj = JSON.parse(jsonWallet);
         expect(parsed).toEqual(obj);
     });
+
+    it('Serialize json sendMsg to protobuf', async () => {
+        let obj = JSON.parse(jsonMsg);
+        let buffer = await serializeSendMsg(obj);
+        let hex = buffer.toString('hex')
+        expect(hex).toEqual(hexMsg);
+    });
+    it('Parse protobuf sendMsg to json', async () => {
+        let buffer = new Buffer(hexMsg, 'hex');
+        let parsed = await parseSendMsg(buffer);
+        let obj = JSON.parse(jsonMsg);
+        expect(parsed).toEqual(obj);
+    });
+
 });
+
+function loadJSON(filename) {
+    let filepath = path.resolve(__dirname, "data", filename) + ".json";
+    let data = fs.readFileSync(filepath, 'utf8')
+                 .replace(/currency_code/g, "currencyCode");
+    return data;
+}
+
+function loadPB(filename) {
+    let filepath = path.resolve(__dirname, "data", filename) + ".bin";
+    let data = fs.readFileSync(filepath, 'hex');
+    return data;
+}
